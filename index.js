6f3ebfd620e870619c0de6cee886e4d8b890138d
unknown <xarmzon@gmail.com>
2022-05-16 14:35:08 +0000

    update

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const mobileMenu = document.querySelector("#mobile-menu");
+const icon = document.querySelector("#mobile-menu i");
 const navItems = document.querySelector("nav ul");
 
 mobileMenu.addEventListener("click", () => {
   navItems.classList.toggle("active");
-  const icon = document.querySelector("#mobile-menu i");
-  console.log(icon);
+  if (icon.classList.contains("bi-list")) {
+    icon.classList.remove("bi-list");
+    icon.classList.add("bi-x-circle");
+  } else {
+    icon.classList.add("bi-list");
+    icon.classList.remove("bi-x-circle");
+  }
 });
