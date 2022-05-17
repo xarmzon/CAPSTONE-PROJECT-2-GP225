da6191c474a0b66886e0ed5c46a02d534e5b35dd
unknown <xarmzon@gmail.com>
2022-05-17 10:20:16 +0000

    updates

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const navItems = document.querySelector("nav ul");
 const form = document.forms[0];
 const alertBox = document.querySelector("div.alert");
 const inputs = document.querySelectorAll("form input.form-control");
-const errors = [];
+
 mobileMenu.addEventListener("click", () => {
   navItems.classList.toggle("active");
   if (icon.classList.contains("bi-list")) {
@@ -16,15 +16,66 @@ mobileMenu.addEventListener("click", () => {
   }
 });
 
-inputs.forEach((item, i) => {
-  item.addEventListener("blur", (e) => {
-    console.log(e.target.name);
-  });
-});
-
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log(form);
   alertBox.classList.remove("d-none");
-  alertBox.innerHTML = "<p>Error</p>";
+
+  inputs.forEach((inp) => {
+    validateInput(inp);
+  });
+
+  const errors = [];
+  inputs.forEach((inp, i) => {
+    errors[i] = inp.nextElementSibling.innerHTML.length > 0;
+  });
+
+  const hasErrors = errors.some((i) => i);
+  if (hasErrors) {
+    alertBox.classList.remove("alert-success");
+    alertBox.classList.add("alert-danger");
+    alertBox.innerText = "Please fix all the errors";
+  } else {
+    alertBox.classList.remove("alert-danger");
+    alertBox.classList.add("alert-success");
+    alertBox.innerText =
+      "Congratulations, your account has been added successfully";
+  }
 });
+
+const validateInput = (ele) => {
+  alertBox.classList.add("d-none");
+  const val = ele.value;
+  const msg = ele.dataset.msg;
+  if (ele.name === "cpass") {
+    const pass = document.querySelector("input[name='pass']");
+    if (pass.value !== val) {
+      handleErrors(ele, msg);
+    } else {
+      handleErrors(ele, "");
+    }
+  } else {
+    const pattern = ele.dataset.pattern;
+    const p = new RegExp(pattern);
+
+    if (!p.test(val)) {
+      handleErrors(ele, msg);
+    } else {
+      handleErrors(ele, "");
+    }
+  }
+};
+
+const handleErrors = (ele, msg) => {
+  const msgBox = ele.nextElementSibling;
+  if (msg.length > 0) {
+    ele.classList.add("is-invalid");
+  } else {
+    ele.classList.remove("is-invalid");
+  }
+  if (msgBox.length > 0) {
+    msgBox.classList.add("invalid-feedback");
+  } else {
+    msgBox.classList.remove("invalid-feedback");
+  }
+  msgBox.innerText = msg;
+};
